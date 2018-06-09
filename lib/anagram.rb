bae650979588b2dbc6b6143c624a34acd8f45c66
Ms-Alex <ms.hernandeza1@gmail.com>
2018-06-09 11:15:55 -0400

    implemented methods for anagram class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+
+
+class Anagram
+
+  attr_accessor :anagram
+  attr_reader :word_list
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(word_list)
+    @word_list = word_list
+    @word_list.select do |word|
+      word.split("").sort == sorted_anagram = anagram.split("").sort
+    end
+  end
+
+end
