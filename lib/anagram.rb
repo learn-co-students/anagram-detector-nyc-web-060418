4f7ef66b981db10a712a22d0ca688538397782df
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-06-11 18:03:50 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array.select {|candidate| candidate.chars.sort == @word.chars.sort}
+  end
+
+end
